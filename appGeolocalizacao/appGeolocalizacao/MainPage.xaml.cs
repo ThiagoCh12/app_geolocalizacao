b2f49a32107cef7bb7ef0249db4a11181898b55c
Aluno <Aluno@SNCTM202LABF09>
2024-02-05 20:11:32 -0300

    Adicionar arquivos de projeto.

---
+++ appGeolocalizacao/appGeolocalizacao/MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace appGeolocalizacao
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+
+        public async Task MostrarMapa()
+        {
+            var location = await Geolocation.GetLocationAsync(new GeolocationRequest()
+            { DesiredAccuracy = GeolocationAccuracy.Best});
+            var locationinfo = new Location(location.Latitude, location.Longitude);
+            var options = new MapLaunchOptions { Name = "Meu local" };
+            await Map.OpenAsync(locationinfo, options);
+
+        }
+        
+        async void btnLocation_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var location = await Geolocation.GetLocationAsync(new GeolocationRequest()
+                { DesiredAccuracy = GeolocationAccuracy.Best });
+                if (location != null) 
+                { 
+                    lblLatitude.Text = "Latitude" + location.Latitude.ToString();
+                    lblLongitude.Text = "Longitude" + location.Longitude.ToString();
+                
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                await DisplayAlert("Falhou", fnsEx.Message, "OK");
+            }
+            catch(PermissionException pEx)
+            {
+                await DisplayAlert("Falhou", pEx.Message, "OK");
+            }
+            catch(Exception er)
+            {
+                await DisplayAlert("Falhou", er.Message, "OK");
+            }   
+        }
+
+        private async void btnVerMapa_Clicked(object sender, EventArgs e)
+        {
+            await MostrarMapa();
+        }
+    }
+}
